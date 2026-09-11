805d9b7934735decfb3cef98c974086853fb635e
c-k-bull <ckbull@umich.edu>
2026-09-11 14:33:26 -0400

    P7d: deploy config

--- db.py
+++ db.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 url = os.getenv("DATABASE_URL", "postgresql+psycopg://localhost/rankmymovies")
+if url.startswith("postgresql://"):
+    url = url.replace("postgresql://", "postgresql+psycopg://", 1)
 engine = create_engine(url, pool_pre_ping=True)
 
 SCHEMA = """
